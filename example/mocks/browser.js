cc6d830f09c76c2e95153301f22d78c241e2ee34
NZ10387 <nz10387@naverz-corp.com>
2024-04-30 11:19:36 +0900

    feat: popover 생성 및 popover body 기본틀 작성

--- example/mocks/browser.js
+++ example/mocks/browser.js
@@ -5,18 +5,35 @@ const { setupWorker, http, HttpResponse } = MockServiceWorker;
 
 // console.dir(MockServiceWorker);
 
-export const worker = () => {
-
-  
-  return setupWorker(
-    ...[
-      http.get('http://www.example.com/user', () => {
-        return HttpResponse.json({
-            id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
-            firstName: 'John',
-            lastName: 'Maverick',
-        });
-      }),
-    ]
-  );
-};
+export const worker = setupWorker(
+  ...[
+    http.get('http://www.example.com/user', () => {
+      return HttpResponse.json({
+        id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+        firstName: 'John',
+        lastName: 'Maverick',
+      });
+    }),
+    http.post('http://www.example.com/user', () => {
+      return HttpResponse.json({
+        id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+        firstName: 'John',
+        lastName: 'Maverick',
+      });
+    }),
+    http.get('http://www.example.com/user2', () => {
+      return HttpResponse.json({
+        id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+        firstName: 'John',
+        lastName: 'Maverick',
+      });
+    }),
+    http.delete('http://www.example.com/user2', () => {
+      return HttpResponse.json({
+        id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+        firstName: 'John',
+        lastName: 'Maverick',
+      });
+    }),
+  ],
+);
